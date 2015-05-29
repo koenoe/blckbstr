e803c06f9ed811eab68b4f231f5a4c53f7bafb88
Koen Romers <info@koenromers.com>
2015-05-29 11:05:40 +0200

    chore(setup): yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blckbstrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the blckbstrApp
+ */
+angular.module('blckbstrApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blckbstrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blckbstrApp
+ */
+angular.module('blckbstrApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
